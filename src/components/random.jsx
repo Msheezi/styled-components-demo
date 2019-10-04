8a150079dd85008a8bed2bccce99013583cb63af
Msheezi <marshall.randolph@gmail.com>
2019-10-03 21:33:36 -0700

    Done for the night

--- src/components/random.jsx
+++ src/components/random.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  height: 600px;
-  width: 600px;
+  height: 100vh;
+  width: 80vw;
   margin: 20px 100px 20px 100px;
   display: flex;
   flex-direction: column;
+  background-color: #ddbde6;
 `;
 
 const Button = styled.button`
@@ -15,6 +16,8 @@ const Button = styled.button`
   border: 2px solid red;
   margin 10px 5px;
 `;
+
+// const box = styled.
 
 export default class Page extends React.Component {
   constructor(props) {
